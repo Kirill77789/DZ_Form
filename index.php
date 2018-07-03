7767883fe8607b52b94d0b416d19cf4558684c10
Kirill <Nordian89@mail.ru>
2018-07-04 01:26:19 +0300

    Форма

--- index.php
+++ index.php
@@ -12,6 +12,51 @@ function echo_var($name){
     return '';
 };
 
+
+function year_word($age){  // Первая функция для написания лет
+    $words = [
+        'лет' => [11, 12, 13, 14, 0, 5, 6, 7, 8, 9,],
+        'год' => [1,],
+        'года' => [2, 3, 4,],
+    ];
+    for ($i=2; $i > 0; $i--){
+        $age = substr($age, -$i);
+        foreach ($words as $word => $years) {
+            foreach ($years as $year) {
+                if ($age == $year) {
+                    return $word;
+                };
+            };
+        };
+    };
+};
+
+function year_word2($age){   //вторая функция для напсиания лет
+    $ar_1 = [11, 12, 13, 14];
+    $ar_2 = [2, 3, 4];
+    if(substr($age,-2, 2)>=11 && substr($age,-2, 2)<=14 ){
+        $znach = substr($age,-2, 2);
+    }else{
+        $znach = substr($age,-1);
+    };
+    switch($znach){
+        case ($ar_1[(array_search($znach, $ar_1))]):
+            return 'лет';
+            break;
+        default:
+            switch ($znach){
+                case 1:
+                    return 'год';
+                    break;
+                case ($ar_2[(array_search($znach, $ar_2))]):
+                    return 'года';
+                    break;
+                default:
+                    return 'лет';
+            };
+    };
+};
+
 function output_txt($redirect = false){
     $out_1 =[];
     $out_2 =[];
@@ -27,21 +72,10 @@ function output_txt($redirect = false){
                             $out_2 []= $_POST['FIO'].'.';
                             break;
                         case 'age':
-                            switch($_POST['age']){
-                                case substr($_POST['age'],-2)==(11||12||13||14) :
-                                    $word ='лет';
-                                    break;
-                                case substr($_POST['age'],-1)==1:
-                                    $word ='год';
-                                    break;
-                                case substr($_POST['age'],-1)==(2||3||4):
-                                    $word ='года';
-                                    break;
-                                default:
-                                    $word ='лет';
-                            };
-                            $out_1 []= 'Вам '.$_POST['age'].' '.$word.'.';
-                            $out_2 []= '('.$_POST['age'].' '.$word.').';
+                            $word_1 = year_word($_POST['age']);
+                            $word_2 = year_word2($_POST['age']);
+                            $out_1 []= 'Вам '.$_POST['age'].' '.$word_1.' (по функции №2- '.$_POST['age'].' '.$word_2.').';
+                            $out_2 []= '('.$_POST['age'].' '.$word_2.').';
                             break;
                         case 'pay':
                             if($_POST['pay'] == 'on'){
@@ -64,7 +98,7 @@ function output_txt($redirect = false){
     $answer = file_get_contents('answer.txt');
     file_put_contents('answer.txt', $answer.$out_2."\n");
     if($redirect == true){
-        header('lication: ?');
+        header('location: ?');
     }else{
         echo $out_1;
     };
